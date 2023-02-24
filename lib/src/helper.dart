8b0c6cacd55c8df5c0629ecc848f6eb93d805272
Eric Bouchard <eric@nextsense.io>
2023-02-24 14:04:52 +0700

    Remove redundant elementAtOrNull extension

--- lib/src/helper.dart
+++ lib/src/helper.dart
@@ -2,11 +2,4 @@ extension CustomList<T> on List<T> {
   List<T> asReversed(bool isReverse) {
     return isReverse ? reversed.toList() : this;
   }
-
-  T? elementAtOrNull(int index) {
-    try {
-      return this[index];
-    } catch (_) {}
-    return null;
-  }
 }
